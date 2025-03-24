946adf53dab032c814e1f1d7b1e568414fd13136
Kristianity23 <christianfloresss23@gmail.com>
2025-03-24 16:49:36 +0800

    Activities

---
+++ Activities/script.js
@@ -0,0 +1,15 @@
+function whatShaIllWear(temp) {
+    //Temperature
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    } else { 
+        console.log("Wear a sweater")
+    }
+
+}
+
+whatShaIllWear(50)
+whatShaIllWear(60)
+whatShaIllWear(80)
