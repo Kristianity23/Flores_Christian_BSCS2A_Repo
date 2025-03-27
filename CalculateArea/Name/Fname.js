93f6cc1c27b8ddf0b15cad17763cb7549a9b0079
Kristianity23 <christianfloresss23@gmail.com>
2025-03-27 17:05:41 +0800

    Funtion Activity

---
+++ CalculateArea/Name/Fname.js
@@ -0,0 +1,6 @@
+function printName (firstname, lastname) {
+    var fullName = firstname + " " + lastname
+    return fullName 
+}
+
+console.log("My Fullname is " + printName("Christian", "Flores"))
